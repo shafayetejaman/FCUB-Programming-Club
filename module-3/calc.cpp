a54e9f9dddf225b1ac1c773e35f5fd5788a7eb0a
shafayetejaman <shafayetejaman@gmail.com>
2024-05-19 16:23:55 +0000

    Sun, May 19, 2024, 10:23 PM +06:00

--- module-3/calc.cpp
+++ module-3/calc.cpp
@@ -6,18 +6,33 @@ int main()
 
     printf("give the value a : ");
     scanf(" %i", &a);
-    printf("give the value a : ");
-    scanf(" %i", &a);
+    
+    printf("give the value b : ");
+    scanf(" %i", &b);
 
     ans = a + b;
     printf("%i + %i = %i\n", a, b, ans);
 
+    printf("give the value a : ");
+    scanf(" %i", &a);
+
+    printf("give the value b : ");
+    scanf(" %i", &b);
     ans = a - b;
     printf("%i - %i = %i\n", a, b, ans);
 
+    printf("give the value a : ");
+    scanf(" %i", &a);
+    printf("give the value b : ");
+    scanf(" %i", &b);
     ans = a * b;
     printf("%i * %i = %i\n", a, b, ans);
 
+    printf("give the value a : ");
+    scanf(" %i", &a);
+
+    printf("give the value b : ");
+    scanf(" %i", &b);
     ans = a / b;
     printf("%i / %i = %i\n", a, b, ans);
 
