99591b51372527ff5041e763e50b2d478ffb5f48
shafayetejaman <shafayetejaman@gmail.com>
2024-05-19 16:41:06 +0000

    Sun, May 19, 2024, 10:41 PM +06:00

--- module-3/calc.cpp
+++ module-3/calc.cpp
@@ -4,25 +4,30 @@ int main()
 {
     int a = 20, b = 10, ans;
 
-    printf("give the value a : ");
-    scanf(" %i", &a);
+    // printf("give the value a : ");
+    // scanf(" %i", &a);
     
-    printf("give the value b : ");
-    scanf(" %i", &b);
+    // printf("give the value b : ");
+    // scanf(" %i", &b);
 
-    ans = a + b;
-    printf("%i + %i = %i\n", a, b, ans);
+    // ans = a + b;
+    // printf("%i + %i = %i\n", a, b, ans);
 
-    ans = a - b;
-    printf("%i - %i = %i\n", a, b, ans);
+    // ans = a - b;
+    // printf("%i - %i = %i\n", a, b, ans);
 
-    ans = a * b;
-    printf("%i * %i = %i\n", a, b, ans);
+    // ans = a * b;
+    // printf("%i * %i = %i\n", a, b, ans);
 
 
 
-    float div_ans = (a * 0.1) / (b * 0.1);
-    printf("%i / %i = %f\n", a, b, div_ans);
+    // float div_ans = (a * 1.0) / (b * 1.0);
+    // printf("%i / %i = %f\n", a, b, div_ans);
+
+    float fl_a = (a * 1.0);
+
+    printf("%f", fl_a);
+
 
 
     return 0;
