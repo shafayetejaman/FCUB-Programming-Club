d2727a8db11395902e8673a85430c547b7f3116c
shafayetejaman <shafayetejaman@gmail.com>
2024-05-19 16:22:18 +0000

    Sun, May 19, 2024, 10:22 PM +06:00

--- module-3/calc.cpp
+++ module-3/calc.cpp
@@ -2,22 +2,22 @@
 
 int main()
 {
-    int a = 20, b= 10, ans;
+    int a = 20, b = 10, ans;
 
-    // printf("give the value ")
+    printf("give the value ");
 
     ans = a + b;
     printf("%i + %i = %i\n", a, b, ans);
 
     ans = a - b;
-    printf("%i + %i = %i\n", a, b, ans);
+    printf("%i - %i = %i\n", a, b, ans);
 
     ans = a * b;
-    printf("%i + %i = %i\n", a, b, ans);
-    
+    printf("%i * %i = %i\n", a, b, ans);
+
     ans = a / b;
-    printf("%i + %i = %i\n", a, b, ans);
-  
+    printf("%i / %i = %i\n", a, b, ans);
+
 
     return 0;
 }
