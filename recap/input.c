283318f320deaaed497ede9a91c82146126ba28b
shafayetejaman <shafayetejaman@gmail.com>
2024-05-21 15:22:50 +0000

    Tue, May 21, 2024, 9:22 PM +06:00

--- recap/input.c
+++ recap/input.c
@@ -2,14 +2,16 @@
 
 int main()
 {
-    int a;
+    int a,b;
 
     scanf("%i", &a);
+    scanf("%i", &b);
 
 
+    float a_in_f = (float)a;
+    float b_in_f = (float)b;
 
-
-    printf("%f this is my money\n", a * 1.0);
+    printf("%f this is my money\n");
 
 
 
