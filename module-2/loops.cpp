45ccad9bfa1a7b03d034be9b9be0d20f0db2988f
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 15:21:57 +0000

    Wed, May 22, 2024, 9:21 PM +06:00

--- module-2/loops.cpp
+++ module-2/loops.cpp
@@ -9,8 +9,8 @@ int main()
     //     printf("%i is equal to 10\n", a);
     //     a++;// 9+1=10
     // }
-
-    for (int a = 1; a <= 10; a++)
+                        //  false
+    for (int a = 1, i = 9; a <= 10 && i < 10; a++, i++)
     {
         printf("%i is equal to 10\n", a);
     }
