ae386532aa1acedd9b160cec177511373ceaabb4
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 14:44:41 +0000

    Wed, May 22, 2024, 8:44 PM +06:00

--- module-2/codition.cpp
+++ module-2/codition.cpp
@@ -3,16 +3,17 @@
 int main()
 {
     int n = 10;
-    
+    int a = 11;
+
       // true
-    if (n == 10)
+    if (n != a)
     {
-        printf("n is equal to 10\n");
+        printf("n is equal to %i\n", a);
     }
     else
     {
         
-        printf("n is not equal to 10\n");
+        printf("n is not equal to %i\n", a);
     }
 
     return 0;
