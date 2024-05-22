989280015beb0802e2d0441a22385149dab5301e
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 14:41:40 +0000

    Wed, May 22, 2024, 8:41 PM +06:00

--- module-2/codition.cpp
+++ module-2/codition.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
-    ret
+    int n = 10;
+    
+      // true
+    if (n == 10)
+    {
+        printf("n is equal to 10\n");
+    }
+    else
+    {
+        
+        printf("n is not equal to 10\n");
+    }
+
+    return 0;
 }
