b83fac9509083ef8c09e0633b34b276bd8593ea1
shafayetejaman <shafayetejaman@gmail.com>
2024-05-22 14:56:45 +0000

    Wed, May 22, 2024, 8:56 PM +06:00

--- module-2/codition.cpp
+++ module-2/codition.cpp
@@ -3,19 +3,18 @@
 int main()
 {
     int n = 20;
-    int a = 30;
+    int a = 20;
 
-    // not true
-    if (n > a)
+    //        and
+    if (n > a && n < a)
     {
         printf("n=%i is greater then a=%i\n", n, a);
     }
     else if (n == a)
     {
-
         printf("n=%i is equal to a=%i\n", n, a);
     }
-    else if (n < a)
+    else if (n <= a)
     {
         printf("n is smaller then a\n");
     }
