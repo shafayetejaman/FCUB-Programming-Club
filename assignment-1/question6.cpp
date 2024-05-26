2ef0efca31e81ec08aa6410cec8cc3f6bdbb2129
shafayetejaman <shafayetejaman@gmail.com>
2024-05-26 16:03:33 +0000

    Sun, May 26, 2024, 10:03 PM +06:00

---
+++ assignment-1/question6.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%i", &n);
+
+    if (n >= 12)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+        
+    }
+
+    return 0;
+}
