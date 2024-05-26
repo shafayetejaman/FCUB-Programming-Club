66bb496075128ff2f42bbf2bd309fe503318f2f7
shafayetejaman <shafayetejaman@gmail.com>
2024-05-26 15:49:02 +0000

    Sun, May 26, 2024, 9:49 PM +06:00

--- assignment-1/question1.cpp
+++ assignment-1/question1.cpp
@@ -7,7 +7,20 @@ int main()
 
     if (money >= 10000)
     {
-        
+        printf("Gucci Bag\n");
+
+        if (money > 20000)
+        {
+            printf("Gucci Belt\n");
+        }
+    }
+    else if (money >= 5000)
+    {
+        printf("Levis Bag\n");
+    }
+    else
+    {
+        printf("Something\n");
     }
 
     return 0;
