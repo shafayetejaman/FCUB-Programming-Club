7ef59fd485e56af49faba73f4d4752862adfcc10
shafayetejaman <shafayetejaman@gmail.com>
2024-07-06 15:42:27 +0000

    Sat, Jul 6, 2024, 9:42 PM +06:00

--- module-2/conditional.c
+++ module-2/conditional.c
@@ -5,14 +5,15 @@ int main()
     int a = 2;
 
 
-    // 2 < 10
-    if (a != 10)
+    // 2 != 10
+    if (a <= 10)
     {
-        printf("a not equal to 10\n");
+        
+        printf("a is equal or grater then 10\n");
     }
     else
     {
-        printf("a is grater then 10\n");
+        printf("a smaller then 10\n");
     }
 
     return 0;
