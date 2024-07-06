9c0a3fc1a7c4fec4567bc4add59b94bdc891d7e2
shafayetejaman <shafayetejaman@gmail.com>
2024-07-06 15:24:03 +0000

    Sat, Jul 6, 2024, 9:24 PM +06:00

--- module-2/varible.c
+++ module-2/varible.c
@@ -4,9 +4,11 @@ int main()
 {
     int a = 10;
     float b = 10.5;
+    char c = 'A';
 
     printf("a = %i\n",a);
-    printf("b = %i\n",b);
+    printf("b = %.2f\n",b);
+    printf("c = %c\n",c);
 
     return 0;
 }
