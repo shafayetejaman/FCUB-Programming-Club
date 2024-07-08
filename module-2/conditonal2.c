8cee02affe3bf63a62ea94ad1993f2c02a545526
shafayetejaman <shafayetejaman@gmail.com>
2024-07-08 10:23:18 +0000

    Mon, Jul 8, 2024, 4:23 PM +06:00

---
+++ module-2/conditonal2.c
@@ -0,0 +1,16 @@
+/*
+    Create a C program that calculates and displays the grade of a student based on their score.The grading criteria are as follows
+*/
+
+
+
+/*
+Create a C program that calculates and displays the grade of a student based on their score.The grading criteria are as follows :
+
+    Score >= 90      : A
+    80 <= Score < 90 : B
+    70 <= Score < 80 : C
+    60 <= Score < 70 : D
+    Score < 60       : F
+
+*/
