61113d81a8b58d6d81828daa6a98b3ee98d3d0a6
shafayetejaman <shafayetejaman@gmail.com>
2024-07-08 10:29:20 +0000

    Mon, Jul 8, 2024, 4:29 PM +06:00

--- module-2/conditonal2.c
+++ module-2/conditonal2.c
@@ -1,18 +1,11 @@
-/*
-    Create a C program that shows if a student pass the exam or not
-
-
-*/
-
 
 
 /*
 Create a C program that calculates and displays the grade of a student based on their score.The grading criteria are as follows :
 
-    79+        : A
-    79 - 60    : B
-    70 - 40 : C
-    60 <= Score < 70 : D
-           : F
+    80=+        : A
+    79 - 60     : B
+    70 - 40     : C
+    39=-        : F
 
 */
